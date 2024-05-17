63e6d1f3f928f268a858b2df5c804874e77342da
Muna Nayak <you@example.com>
2024-05-17 12:34:54 +0530

    js file add

---
+++ task-1/operation.js
@@ -0,0 +1,16 @@
+let num1 = 12;
+let num2 = 2;
+let addition = num1 + num2;
+console.log(addition); // addtion
+
+let substraction = num1 - num2;
+console.log(substraction); // substraction
+
+let multipication = num1 * num2;
+console.log(multipication);// multipication
+
+let division = num1 / num2;
+console.log(division); // division
+
+let modulus = num1 % num2;
+console.log(modulus); // modulus
