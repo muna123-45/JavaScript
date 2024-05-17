83aa1e850817f858e80b74ec84a64224436ea3e9
Muna Nayak <you@example.com>
2024-05-17 12:02:36 +0530

    js file added

---
+++ class-1/01-variable.js
@@ -0,0 +1,21 @@
+//declare a variable
+
+let num = 100;
+console.log(num); //to print the output
+
+let Num = 50;
+console.log(Num);
+
+//space does not allow while declaring variable
+//let second num =1000;(Example)
+
+let second_num = 5000; //second_num is known as (snake case)
+console.log(second_num);
+
+let secondNum = 300;
+console.log(secondNum); //secondNum is known as (camel case)
+
+let float_num = 15.05;
+console.log(float_num);
+
+console.log(typeof float_num);
